b3742d0d18f6e7ba5c6867e5466e822dbef77443
Yuriy Rogachev <y_rogachev@inbox.ru>
2020-06-26 02:28:44 +0300

    api

--- api/api.py
+++ api/api.py
@@ -1,6 +1,41 @@
+from uuid import uuid4
+
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+notes_dict = {
+    "eeea632e-c9ca-449b-93fc-fc084682c002": {
+        "title": "example note title",
+        "body": "expample note body",
+        "tags": ["tag1", "tag2", "tag3"]
+    }
+}
+
+
+@app.route('/notes')
+def notes():
+    return notes_dict
+
+
+@app.route('/notes', methods=["POST"])
+def add_note():
+    id = str(uuid4())
+    notes_dict[id] = request.json
+    return "ok"
+
+
+@app.route('/notes/<note_id>', methods=["DELETE"])
+def del_note(note_id):
+    if not note_id in notes_dict:
+        return "Note id not in dict"
+    del notes_dict[note_id]
+    return "ok"
+
+
+@app.route('/notes/<note_id>', methods=["PUT"])
+def update_note(note_id):
+    if not note_id in notes_dict:
+        return "Note id not in dict"
+    notes_dict[note_id] = request.json
+    return "ok"
